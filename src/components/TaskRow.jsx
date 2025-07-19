30238c2f98f22ea148f32cfe974308804e13d49f
RosyPetrella <petrellamr@gmail.com>
2025-07-19 12:30:29 +0200

    feat: implement TaskRow component and integrate it into TaskList for task display

---
+++ src/components/TaskRow.jsx
@@ -0,0 +1,16 @@
+function TaskRow({ title, status, createdAt }) {
+  let bgColor;
+
+  if (status === "To do") bgColor = "red";
+  else if (status === "Doing") bgColor = "yellow";
+  else if (status === "Done") bgColor = "green";
+  return (
+    <tr>
+      <td>{title}</td>
+      <td style={{ backgroundColor: bgColor }}>{status}</td>
+      <td>{createdAt}</td>
+    </tr>
+  );
+}
+
+export default React.memo(TaskRow);
--- src/pages/TaskList.jsx
+++ src/pages/TaskList.jsx
@@ -1,7 +1,32 @@
+import TaskRow from "../components/TaskRow";
+import { GlobalContext } from "../context/GlobalContext";
+import { useContext } from "react";
 export default function TaskList() {
+  const { tasks } = useContext(GlobalContext);
   return (
     <>
-      <h1>Lista dei task</h1>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Stato</th>
+              <th>Data di Creazione</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tasks.map((task) => (
+              <TaskRow
+                key={task.id}
+                title={task.title}
+                status={task.status}
+                createdAt={task.createdAt}
+                description={task.description}
+              />
+            ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
